1d6c4231087e18537b4c19cc23e4a8927e221dcc
funkyfall <funkaa15@gmail.com>
2024-08-02 15:00:43 +0300

    added jdbc

--- src/main/java/com/example/spring_boot/TLabApplication.java
+++ src/main/java/com/example/spring_boot/TLabApplication.java
@@ -1,13 +1,16 @@
 package com.example.spring_boot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TLabApplication {
-
+public class TLabApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(TLabApplication.class, args);
 	}
-
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello");
+	}
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,21 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        ConnectionUtil db = new ConnectionUtil();
+
+        Connection conn = db.connect_to_db("TDB", "postgres", "DBPASSWORD");
+        String query = "SELECT * FROM test";
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        rs.next();
+        System.out.println(rs.getString(1));
+        rs.next();
+        
     }
 }
